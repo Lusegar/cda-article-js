425913c2e69535341c0d1d558970ebba7cc693bf
Lusegar <lucas.seggar@epitech.eu>
2023-11-21 16:10:45 +0100

    [ADD] projet cda

---
+++ js/model.js
@@ -0,0 +1,7 @@
+export const Date_now = {
+    builder() {
+        console.log("Hello");
+    },
+    dates: new Date().getFullYear(),
+    el: document.querySelector("time")
+};
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { Date_now } from "./model.js"
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    console.log(e.target)
+    Date_now.builder()
+    //ajouter l import
+    console.log(Date_now.dates)
+    //ajoute la date
+    Date_now.el.innerText = Date_now.dates
+    //ajoute la date dans l'attribut datetime
+    Date_now.el.setAttribute("datetime", Date_now.dates)
+})
